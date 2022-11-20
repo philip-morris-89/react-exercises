cf2eece204a899a3b7f69826baf665003042dc7b
Filippo Scelfo <scelfofilippo@gmail.com>
2022-11-20 18:56:22 +0100

    Add a login button to the Login component. This button should be disabled as long as the username and password inputs are empty. When clicked, the event handler attached to the button should call an onLogin function passed as a prop to the Login component, passing it the state.

--- my-app/src/Login.js
+++ my-app/src/Login.js
@@ -5,6 +5,7 @@ class Login extends React.Component {
     username: '',
     password: '',
     remember: false,
+    loginDisabled: true,
   }
 
   handleInputChange = (event) => {
@@ -12,9 +13,20 @@ class Login extends React.Component {
     const name = event.target.name;
     const type = event.target.type;
     const checked = event.target.checked;
+    const disabled = event.target.disabled;
 
     this.setState({
       [name]: type === 'checkbox' ? checked : value,
+      [name]: type === 'submit' ? disabled : value,
+      loginDisabled: (this.state.username && this.state.password) === '' ? true : false,
+    })
+  }
+
+  onLogin = (event) => {
+    this.setState({
+      username: this.state.username,
+      password: this.state.password,
+      remember: this.state.remember,
     })
   }
 
@@ -26,6 +38,7 @@ class Login extends React.Component {
           <input type="text" name="username" placeholder="Username" value={this.state.username} onChange={this.handleInputChange} />
           <input type="password" name="password" placeholder="Password" value={this.state.password} onChange={this.handleInputChange} />
           <input type="checkbox" name="remember" checked={this.state.remember} onChange={this.handleInputChange} />
+          <button type="submit" name="button" disabled={this.state.loginDisabled} onChange={this.handleInputChange} onClick={this.onLogin}>Login</button>
         </form>
       </div>
     )
