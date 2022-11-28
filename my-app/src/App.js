52cdb93f49790ae18501d058d7571942fe77d859
Filippo Scelfo <scelfofilippo@gmail.com>
2022-11-28 10:46:45 +0100

    Testing

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Container from './Container';
 import { ClickCounter } from './ClickCounter';
+import Counter from './Counter';
+
 
 export class App extends React.Component {
   render() {
@@ -8,6 +10,7 @@ export class App extends React.Component {
       <div>
         <Container title={<h1>My App</h1>}>
           <ClickCounter />
+          <Counter />
         </Container>
       </div>
     )
