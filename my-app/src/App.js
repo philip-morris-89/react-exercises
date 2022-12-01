756134b15bcc7212b03d29ebd89f47d455688e94
Filippo Scelfo <scelfofilippo@gmail.com>
2022-12-01 17:44:01 +0100

    created branch to exercise

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,12 @@
-import {useState} from 'react';
 import Container from './Container';
-import { GithubUser } from './GithubUser';
+import DisplayLanguage from './DisplayLanguage';
 
 
 export function App() {
-  const [username, setUsername] = useState('')
-
     return (
       <div>
         <Container title={<h1>My App</h1>}>
-          <h1>Fetch the data of Github user</h1>
-          <input placeholder='Enter Github user' value={username} onChange={(e) => setUsername(e.target.value)} />
-          <br/>
-          <br/>
-          <GithubUser username={username} />
+          <DisplayLanguage />
         </Container>
       </div>
     )
